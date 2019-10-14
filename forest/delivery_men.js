58a35bc6d8e9af95cf3671e21312a46b7472bae8
Leo <coder.leo.le@gmail.com>
2019-10-14 16:23:48 +0700

    Smart field update value

--- forest/delivery_men.js
+++ forest/delivery_men.js
@@ -8,6 +8,14 @@ Liana.collection('delivery_men', {
       get: (men) => {
         return men.firstname + ' ' + men.lastname;
       },
+      set: (men, fullname) => {
+        let names = fullname.split(' ');
+        men.firstname = names[0];
+        men.lastname = names[1];
+
+        // Don't forget to return the customer.
+        return men;
+      },
     },
   ],
 })
